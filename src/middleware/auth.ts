490e9a1c9f470091cba0ff991769f9b765477c99
Sayan3sarkar <sayansarkar333@gmail.com>
2022-10-21 23:56:14 +0530

    Updates

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
-import { config } from "../config";
+import { config } from "../config/config";
 import { NotAuthorizedError } from "../errors/not-authorized-error";
 import { UserPayload } from "../types";
 
